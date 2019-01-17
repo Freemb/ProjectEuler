9216c8a2cb5292bb5eb9a7e65bdf2c1baeeabba3
Michael Bent <Michaelobent@gmail.com>
2019-01-17 23:30:31 +0000

    Completed problem 4

--- solutions/Problems.cs
+++ solutions/Problems.cs
@@ -9,10 +9,14 @@ namespace ProjectEuler.Solutions
 	class Problems
 	{
 		private int number;
+		/// <summary>
+		/// Constructor initiates console to select problem number to be solved
+		/// </summary>
 		public Problems()
 		{
-			Console.WriteLine("What is the problem number?");
+			Console.WriteLine("Enter the problem number you wish to solve");
 			int.TryParse(Console.ReadLine(), out number);
+			Console.Clear();
 
 			switch (number)
 			{
@@ -25,6 +29,9 @@ namespace ProjectEuler.Solutions
 				case 3:
 					Problem3();
 					break;
+				case 4:
+					Problem4();
+					break;
 				default:
 					Console.WriteLine("This problem has not been solved yet");
 					break;
@@ -36,6 +43,8 @@ namespace ProjectEuler.Solutions
 		/// </summary>
 		private void Problem1()
 		{
+			Console.WriteLine("Problem 1: Find the sum of all the multiples of 3 or 5 below 1000.");
+			
 			List<int> Results = new List<int>(); // list for all the multiples
 			for (int i = 0; i < 1000; i++)
 			{
@@ -44,7 +53,7 @@ namespace ProjectEuler.Solutions
 					Results.Add(i);
 				}
 			}
-			Console.WriteLine("The answer is " + Results.Sum());
+			Console.WriteLine("The answer is {0}",Results.Sum());
 			Console.ReadLine();
 		}
 
@@ -53,6 +62,8 @@ namespace ProjectEuler.Solutions
 		/// </summary>
 		private void Problem2()
 		{
+			Console.WriteLine("Problem 2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.");
+		
 			List<int> Resultset = new List<int>(); // list to store even fibonacci numbers
 			int[] Fibonacci = new int[3]{1,2,0}; // array that holds 3 consecutive values of the fibonacci sequence
 			Resultset.Add(2);
@@ -67,7 +78,7 @@ namespace ProjectEuler.Solutions
 				Fibonacci[0] = Fibonacci[1];
 				Fibonacci[1] = Fibonacci[2];
 			} while (Fibonacci[2] <= 4000000);
-			Console.WriteLine(Resultset.Sum());
+			Console.WriteLine("The solution is {0}",Resultset.Sum());
 			Console.ReadLine();
 
 		}
@@ -78,8 +89,9 @@ namespace ProjectEuler.Solutions
 		/// </summary>
 		private void Problem3()
 		{
+			Console.WriteLine("Problem 3: What is the largest prime factor of the number 600851475143 ?");
 			int i = 2;
-			long number = 600851475143;
+			long number = 600851475143L;
 			while (i !=number)
 			{
 				if (number % i == 0) // test for prime factors i of number
@@ -88,11 +100,41 @@ namespace ProjectEuler.Solutions
 				}
 				i++;
 			}
-			Console.WriteLine("The result is " + number);
+			Console.WriteLine("The result is {0}",number);
 			Console.ReadLine();
 		}
 
+		//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+		/// <summary>
+		///Find the largest palindrome made from the product of two 3-digit numbers.
+		/// </summary>
+		private void Problem4()
+		{
+			Console.WriteLine("Problem 4: Find the largest palindrome made from the product of two 3-digit numbers.");
 
+			char[] ToReverse;
+			int product;
+			string Reversed = string.Empty; 
+			List<int> palindromes = new List<int>();
+
+			for (int i = 999; i > 0; i--)  //will iterate backwards from 999 and find product
+			{
+				for (int j = 999; j > 0; j--)
+				{
+					product = i * j;
+					ToReverse = product.ToString().ToCharArray();
+					//Reversed = (new string(Array.Reverse(ToReverse)); //could use this to simplify
+					for (int z = ToReverse.Length - 1; z >-1; z--) //adds each character in array from last to first
+					{
+						Reversed += ToReverse[z];
+					}
+					if (int.Parse(Reversed) == product){ palindromes.Add(product);}
+					Reversed = string.Empty; // needed to reset the string for new reversal
+				}
+			}
+			Console.WriteLine("The largest palindrome is {0}, the smallest is {1} ",palindromes.Max(),palindromes.Min());
+			Console.ReadLine();
+		}
 
 
 
