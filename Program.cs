2967714e5f2e13c71f0267cb68d48df4b20471a3
Michael Bent <Michaelobent@gmail.com>
2019-02-03 13:10:46 +0000

    Moved problem selection to sonctructor of problem class. Changed return types to string from void.

--- Program.cs
+++ Program.cs
@@ -3,61 +3,13 @@ using ProjectEuler.Solutions;
 
 namespace ProjectEuler
 {
-	class Program
+	public class Program
 	{
 		private static int number;
 		static void Main(string[] args)
 		{
 			Problems Problemset = new Problems();
-			Console.WriteLine("Enter the problem number you wish to solve");
-			while (!int.TryParse(Console.ReadLine(), out number))
-			{
-				Console.Clear();
-				Console.WriteLine("Please enter a valid problem number");
-				continue;
-			}
-
-
-			switch (number)
-			{
-				case 1:
-					Problemset.Problem1();
-					break;
-				case 2:
-					Problemset.Problem2();
-					break;
-				case 3:
-					Problemset.Problem3();
-					break;
-				case 4:
-					Problemset.Problem4();
-					break;
-				case 5:
-					Problemset.Problem5();
-					break;
-				case 6:
-					Problemset.Problem6();
-					break;
-				case 7:
-					Problemset.Problem7();
-					break;
-				case 8:
-					Problemset.Problem8();
-					break;
-				case 9:
-					Problemset.Problem9();
-					break;
-				case 10:
-					Problemset.Problem10();
-					break;
-				case 11:
-					Problemset.Problem11();
-					break;
-				default:
-					Console.WriteLine("This problem has not been solved yet");
-					Console.ReadLine();
-					break;
-			}
 		}
 	}
+
 }
--- solutions/Problems.cs
+++ solutions/Problems.cs
@@ -6,12 +6,68 @@ using System.Diagnostics;
 
 namespace ProjectEuler.Solutions
 {
-	struct Problems
+	public class Problems
 	{
+		private int number;
+		public Problems()
+		{
+			Console.WriteLine("Enter the problem number you wish to solve");
+			while (!int.TryParse(Console.ReadLine(), out number))
+			{
+				Console.Clear();
+				Console.WriteLine("Please enter a valid problem number");
+				continue;
+			}
+		Selectproblem:
+			switch (number)
+			{
+				case 1:
+					Console.WriteLine(Problem1());
+					break;
+				case 2:
+					Console.WriteLine(Problem2());
+					break;
+				case 3:
+					Console.WriteLine(Problem3());
+					break;
+				case 4:
+					Console.WriteLine(Problem4());
+					break;
+				case 5:
+					Console.WriteLine(Problem5());
+					break;
+				case 6:
+					Console.WriteLine(Problem6());
+					break;
+				case 7:
+					Console.WriteLine(Problem7());
+					break;
+				case 8:
+					Console.WriteLine(Problem8());
+					break;
+				case 9:
+					Console.WriteLine(Problem9());
+					break;
+				case 10:
+					Console.WriteLine(Problem10());
+					break;
+				case 11:
+					Console.WriteLine(Problem11());
+					break;
+				case 12:
+					Console.WriteLine(Problem12());
+					break;
+				default:
+					Console.WriteLine("This problem has not been solved yet");
+					break;
+			}
+			Console.WriteLine("\nEnter a number if you wish to solve another problem or any letter to quit");
+			if (int.TryParse(Console.ReadLine(), out number)) { goto Selectproblem; };
+		}
 		/// <summary>
 		/// Find the sum of all the multiples of 3 or 5 below 1000.
 		/// </summary>
-		public void Problem1()
+		private string Problem1()
 		{
 			Console.WriteLine("Problem 1: Find the sum of all the multiples of 3 or 5 below 1000.");
 
@@ -23,14 +79,14 @@ namespace ProjectEuler.Solutions
 					Results.Add(i);
 				}
 			}
-			Console.WriteLine("The answer is {0}", Results.Sum());
-			Console.ReadLine();
+			//Console.WriteLine("The answer is {0}", Results.Sum());
+			return $"The answer is {Results.Sum()}";
 		}
 
 		/// <summary>
 		/// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 		/// </summary>
-		public void Problem2()
+		private string Problem2()
 		{
 			Console.WriteLine("Problem 2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.");
 
@@ -48,16 +104,14 @@ namespace ProjectEuler.Solutions
 				Fibonacci[0] = Fibonacci[1];
 				Fibonacci[1] = Fibonacci[2];
 			} while (Fibonacci[2] <= 4000000);
-			Console.WriteLine("The solution is {0}", Resultset.Sum());
-			Console.ReadLine();
-
+			return $"The solution is {Resultset.Sum()}";
 		}
 
 		//The prime factors of 13195 are 5, 7, 13 and 29.
 		/// <summary>
 		/// What is the largest prime factor of the number 600851475143 ?
 		/// </summary>
-		public void Problem3()
+		private string Problem3()
 		{
 			Console.WriteLine("Problem 3: What is the largest prime factor of the number 600851475143 ?");
 			int i = 2;
@@ -70,15 +124,14 @@ namespace ProjectEuler.Solutions
 				}
 				i++;
 			}
-			Console.WriteLine("The result is {0}", number);
-			Console.ReadLine();
+			return $"The result is {number}";
 		}
 
 		//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 		/// <summary>
 		///Find the largest palindrome made from the product of two 3-digit numbers.
 		/// </summary>
-		public void Problem4()
+		private string Problem4()
 		{
 			Console.WriteLine("Problem 4: Find the largest palindrome made from the product of two 3-digit numbers.");
 
@@ -93,7 +146,7 @@ namespace ProjectEuler.Solutions
 				{
 					product = i * j;
 					ToReverse = product.ToString().ToCharArray();
-					//Reversed = (new string(Array.Reverse(ToReverse)); //could use this to simplify
+					//Reversed = new string(Array.Reverse(ToReverse); //could use this to simplify
 					for (int z = ToReverse.Length - 1; z > -1; z--) //adds each character in array from last to first
 					{
 						Reversed += ToReverse[z];
@@ -102,14 +155,13 @@ namespace ProjectEuler.Solutions
 					Reversed = string.Empty; // needed to reset the string for new reversal
 				}
 			}
-			Console.WriteLine("The largest palindrome is {0}, the smallest is {1} ", palindromes.Max(), palindromes.Min());
-			Console.ReadLine();
+			return $"The largest palindrome is {palindromes.Max()}";
 		}
 		/// <summary>
 		/// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 		/// </summary>
 
-		public void Problem5()
+		private string Problem5()
 		{
 			Console.WriteLine("What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?");
 			long Results = 20; // inititialised to smallest number that is a multiple of the largest number in the set of numbers
@@ -136,14 +188,13 @@ namespace ProjectEuler.Solutions
 				}
 
 			}
-			Console.WriteLine("The smallest multiple of all the numbers is {0}", Results);
-			Console.ReadLine();
+			return $"The smallest multiple of all the numbers is {Results}";
 		}
 
 		/// <summary>
 		/// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 		/// </summary>
-		public void Problem6()
+		private string Problem6()
 		{
 			Console.WriteLine("Problem 6:Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. ");
 			int sum = 0;
@@ -155,14 +206,13 @@ namespace ProjectEuler.Solutions
 
 			}
 			long result = (sum * sum) - sumsquare;
-			Console.WriteLine("The result is {0}", result);
-			Console.ReadLine();
+			return $"The result is {result}";
 		}
 		/// <summary>
 		/// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 		///What is the 10 001st prime number?
 		/// </summary>
-		public void Problem7()
+		private string Problem7()
 		{
 			Console.WriteLine("Problem 7: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13." +
 								"What is the 10 001st prime number?");
@@ -184,15 +234,13 @@ namespace ProjectEuler.Solutions
 
 				number += 2;
 			}
-			Console.WriteLine("The result is {0}", result[10000]);
-			Console.ReadLine();
+			return $"The result is {result[10000]}";
 		}
 		/// <summary>
 		/// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 		/// </summary>
-		public void Problem8()
+		private string Problem8()
 		{
-			Console.WriteLine("Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?");
 			int adjacent = 13;
 			int result = 0;
 			string Result = string.Empty;
@@ -206,6 +254,8 @@ namespace ProjectEuler.Solutions
 							"1786645835912456652947654568284891288314260769004224219022671055626321111109370544217506941658960408" +
 							"0719840385096245544436298123098787992724428490918884580156166097919133875499200524063689912560717606" +
 							"0588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+			Console.WriteLine("Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?" + $"\n{number}\n");
 
 			for (int i = 0; i + adjacent <= number.Length; i++)
 			{
@@ -232,46 +282,44 @@ namespace ProjectEuler.Solutions
 				}
 			}
 
-			Console.WriteLine("The result string is {0} and the value of the product is {1}", Result, result);
-			Console.ReadLine();
-
+			return $"The result string is {Result} and the value of the product is {result}";
 		}
-		
-		public void Problem9()
+		/// <summary>
+		/// A Pythagorean triplet is a set of three natural numbers, a less than b less than c, for which a2 + b2 = c2.
+		///For example, 32 + 42 = 9 + 16 = 25 = 52.There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+		///Find the product abc.
+		/// </summary>
+		private string Problem9()
 		{
+			Console.WriteLine(" A Pythagorean triplet is a set of three natural numbers, a less than b less than c, for which a2 + b2 = c2.\n" +
+								"For example, 32 + 42 = 9 + 16 = 25 = 52.There exists exactly one Pythagorean triplet for which a + b + c = 1000.\n" +
+								"Find the product abc.");
 			long product;
 			int sum = 1000;
-			bool flag = false;
-			while (!flag)
-			{
+
 				for (int a = 1; a < sum; a++)
 				{
-					if (flag) { break; }
 					// since a<b<c then b must be less than c which is sum-a-b since a+b+c = sum
 					for (int b = a + 1; b < sum - a - b; b++)
 					{
 						int c = sum - a - b;
 						if (c * c == a * a + b * b)
 						{
-							flag = true;
 							product = a * b * c;
-							Console.WriteLine("The Pythagorean triplets are {0}, {1}, {2}. The Product is {3}", a, b, c, product);
-							break;
+							return $"The Pythagorean triplets are {a}, {b}, {c}. The Product is {product}";
 						}
 					}
 				}
-			}
-			Console.ReadKey();
+			return $"No Pythagorean triplets found that sum to {sum}";
 		}
 		/// <summary>
 		/// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. Find the sum of all the primes below two million.
 		/// </summary>
-		public void Problem10()
+		private string Problem10()
 		{
 			int limit = 2000000;
 			bool flag; //true when number prime
-			List<int> result = new List<int>();
-			result.Add(2); //initialise list with 2
+			List<int> result = new List<int>() { 2 }; // initialise list with number 2;
 			int number = 3; // starting with the 1st odd number after 2
 			long sum = 0; // doing list.sum() at end costs double the time
 			Stopwatch timer = new Stopwatch();
@@ -289,81 +337,96 @@ namespace ProjectEuler.Solutions
 				number += 2;
 			}
 			timer.Stop();
-			Console.WriteLine("The sum of the primes below {0} is {1}, completed in {2} ms", limit, sum, timer.ElapsedMilliseconds);
-			Console.ReadKey();
+			return $"The sum of the primes below {limit} is {sum}, completed in {timer.ElapsedMilliseconds} ms";
 		}
 		/// <summary>
-		/// 2 dimensional grid
+		/// What is the largest product of 4 adjacent numbers in a 2 dimensional grid?
 		/// </summary>
-		public void Problem11()
+		private string Problem11()
 		{
-			string sequence ="";
+			char[] separators = new char[] { ' ', '\n' };
+			string maxsequence ="";
 			long maxProduct = 1;
 			int[,] intgrid = new int[20, 20];
-			string gridvalues = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 " +
-								"49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 " +
-								"81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 " +
-								"52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 " +
-								"22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 " +
-								"24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 " +
-								"32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 " +
-								"67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 " +
-								"24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 " +
-								"21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 " +
-								"78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 " +
-								"16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 " +
-								"86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 " +
-								"19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 " +
-								"04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 " +
-								"88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 " +
-								"04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 " +
-								"20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 " +
-								"20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 " +
+			string gridvalues = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n" +
+								"49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n" +
+								"81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n" +
+								"52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n" +
+								"22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n" +
+								"24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n" +
+								"32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n" +
+								"67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\n" +
+								"24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\n" +
+								"21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\n" +
+								"78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92\n" +
+								"16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57\n" +
+								"86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\n" +
+								"19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40\n" +
+								"04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66\n" +
+								"88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69\n" +
+								"04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36\n" +
+								"20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16\n" +
+								"20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\n" +
 								"01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
 
-			string[] stringgrid = gridvalues.Split(' ');
+			Console.WriteLine(gridvalues + "\n");
+			string[] stringgrid = gridvalues.Split(separators);
+			
 			for (int i = 0; i < stringgrid.Length; i++) // splits input 1-D string array into integer 2-D array
 			{
 				intgrid[i / 20, i % 20] = int.Parse(stringgrid[i]); //remainder gives column number
 			}
-			//only 1 direction for each plane needs to be condisdered as there will be an equivalent in the other direction
+			//only 1 direction for each needs to be condisdered as there will be an equivalent in the other direction
 			for (int i = 0; i < 20; i++) 
 			{
 				for (int j = 0; j < 20; j++)
 				{
 					long horizproduct = 1;
 					long vertproduct = 1;
-					long diagrightproduct = 1;
-					long diagleftproduct = 1;
-					string horizsequence = "";
-					string vertsequence = "";
-					string diagleftsequence = "";
-					string diagrightsequence = "";
+					long downRightproduct = 1;
+					long downLeftproduct = 1;
+					int[] horizsequence = new int[4];
+					int[] vertsequence = new int[4];
+					int[] downLeftsequence = new int[4];
+					int[] downRightsequence = new int[4]; 
 
 					for (int k = 0; k < 4; k++)
 					{
-						horizproduct *= j < 17 ? intgrid[i, j + k] : 0;
-						horizsequence += j < 17 ? intgrid[i, j + k].ToString() + " X " : "";
-
-						vertproduct *= i < 17 ? intgrid[i + k, j] : 0;
-						vertsequence += i < 17 ? intgrid[i + k, j].ToString() + " X " : "";
-
-						diagrightproduct *= i < 17 && j < 17 ? intgrid[i + k, j + k] : 0;
-						diagrightsequence += i < 17 && j < 17 ? intgrid[i + k, j + k].ToString() + " X " : "";
-
-						diagleftproduct *= i < 17 && j > 2 ? intgrid[i + k, j - k] : 0;
-						diagleftsequence += i < 17 && j > 2 ? intgrid[i + k, j - k].ToString() + " X " : "";
-
+						if (j < 17) // check horizontally stopping at 17th column so not out of bounds
+						{
+							horizsequence[k] = intgrid[i, j + k];
+							horizproduct *= horizsequence[k];
+						}
+						if (i < 17) //check vertically stopping at 17th row so not out of bounds
+						{
+							vertsequence[k] = intgrid[i + k, j];
+							vertproduct *= vertsequence[k];
+						}
+						if (i < 17 && j < 17) //check diagonally down right stopping at 17th row and column so not out of bounds
+						{
+							downRightsequence[k] = intgrid[i + k, j + k];
+							downRightproduct *= downRightsequence[k];
+							
+						}
+						if (j > 2 && i < 17) //check diagonally down left starting at 4th column and stopping at 17th row so not out of bounds
+						{
+							downLeftsequence[k] = intgrid[i + k, j - k];
+							downLeftproduct *= downLeftsequence[k];
+						}
 					}
-					if (horizproduct > maxProduct) { maxProduct = horizproduct; sequence = horizsequence; }
-					if (vertproduct > maxProduct) { maxProduct = vertproduct; sequence = vertsequence; }
-					if (diagrightproduct > maxProduct) { maxProduct = diagrightproduct; sequence = diagrightsequence; }
-					if (diagleftproduct > maxProduct) { maxProduct = diagleftproduct; sequence = diagleftsequence; }
+					if (horizproduct > maxProduct) { maxProduct = horizproduct; maxsequence = "Horizontal: " + string.Join(" X ",horizsequence); }
+					if (vertproduct > maxProduct) { maxProduct = vertproduct; maxsequence = "Vertical: " + string.Join(" X ", vertsequence); }
+					if (downRightproduct > maxProduct) { maxProduct = downRightproduct; maxsequence = "Down Right: " + string.Join(" X ",downRightsequence); }
+					if (downLeftproduct > maxProduct) { maxProduct = downLeftproduct; maxsequence = "Down Left: " + string.Join(" X ",downLeftsequence); }
 				}
 				
 			}
-			Console.WriteLine("The largest sequence is {1}, its product is {0}", maxProduct, sequence.Substring(0, sequence.Length - " X ".Length));
-			Console.ReadKey();
+			return $"The largest sequence is {maxsequence}, its product is {maxProduct}";
+		}
+
+		public string Problem12()	 
+		{
+			return "";
 		}
 
 
